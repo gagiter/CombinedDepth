8da58d41a14c96133a778d01ecb3421b600c4f4d
gagiter <gagiter@163.com>
2020-04-05 16:04:20 +0800

    depth

--- criteria.py
+++ criteria.py
@@ -1,5 +1,15 @@
 
+import torch.nn
 
 
-class Criteria:
-    pass
+class Criteria(torch.nn.Module):
+    def __init__(self):
+        super(Criteria, self).__init__()
+
+    def forward(self, data_in, data_out):
+        depth_in = data_in['depth']
+        mask = data_in['mask']
+        depth_out = data_out['depth']
+        loss = ((depth_in - depth_out) * mask).abs().sum() / mask.sum()
+        return loss
+
