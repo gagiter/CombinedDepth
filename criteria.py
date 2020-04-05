028a5bbec736cf80590df71214227780da287a83
gagiter <gagiter@163.com>
2020-04-06 07:13:34 +0800

    loss ref

--- criteria.py
+++ criteria.py
@@ -7,9 +7,24 @@ class Criteria(torch.nn.Module):
         super(Criteria, self).__init__()
 
     def forward(self, data_in, data_out):
-        depth_in = data_in['depth']
-        mask = data_in['mask']
-        depth_out = data_out['depth']
-        loss = ((depth_in - depth_out) * mask).abs().sum() / mask.sum()
+        loss = 0.0
+        if 'depth' in data_in:
+            depth_in = data_in['depth']
+            mask = data_in['mask']
+            depth_out = data_out['depth']
+            loss_depth = ((depth_in - depth_out) * mask).abs().sum() / mask.sum()
+            loss += loss_depth
+
+        for ref in ['stereo', 'previous', 'next']:
+            if ref in data_in:
+                image = data_in['image']
+                image_ref = data_in[ref]
+                depth = data_out['depth'] * 80.0
+                camera = data_out['camera']
+                motion_stereo = data_out['motion' + ref]
+                warp_ref = util.warp(image_ref, depth, camera, motion_stereo)
+                loss_ref = ((image - warp_ref) * mask).abs().sum() / mask.sum()
+                loss += loss_ref
+
         return loss
 
