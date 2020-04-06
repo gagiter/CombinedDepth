bc62e377d2a7d24ecf7cbacbfc8a76d1c940c4ea
gagiter <gagiter@163.com>
2020-04-06 22:37:44 +0800

    warp out

--- criteria.py
+++ criteria.py
@@ -12,7 +12,9 @@ class Criteria(torch.nn.Module):
         depth_out = data_out['depth']
         if 'depth' in data_in:
             depth_in = data_in['depth']
-            mask = data_in['mask']
+            mask = depth_in > 0.1
+            mask &= depth_in < 80.0
+            data_in['mask'] = mask
             loss_depth = ((depth_in - depth_out) * mask).abs().sum() / mask.sum()
             loss += loss_depth
 
@@ -24,6 +26,7 @@ class Criteria(torch.nn.Module):
                 camera = data_out['camera']
                 motion = data_out['motion_' + ref]
                 warp = util.warp(image_ref, depth_out, camera, motion)
+                data_out['warp_' + ref] = warp
                 loss_ref = ((image - warp) * mask).abs().sum() / mask.sum()
                 # loss += loss_ref
 
