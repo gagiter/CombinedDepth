411d168b0ee20d9729bb13ab4c21b4b61324866b
gagiter <gagiter@163.com>
2020-04-07 21:50:59 +0800

    mini batch

--- train.py
+++ train.py
@@ -16,7 +16,8 @@ parser = argparse.ArgumentParser(description='CombinedDepth')
 parser.add_argument('--data_root', type=str, default='data')
 parser.add_argument('--encoder', type=str, default='resnet34')  # mobilenet_v2
 parser.add_argument('--model_name', type=str, default='model')
-parser.add_argument('--batch_size', type=int, default=2)
+parser.add_argument('--batch_size', type=int, default=8)
+parser.add_argument('--mini_batch_size', type=int, default=2)
 parser.add_argument('--resume', action='store_true', default=False)
 parser.add_argument('--no_cuda', action='store_true', default=False)
 parser.add_argument('--gpu_id', type=int, default=1)
@@ -26,6 +27,11 @@ parser.add_argument('--epoch_start', type=int, default=0)
 parser.add_argument('--epoch_num', type=int, default=50000)
 parser.add_argument('--summary_freq', type=int, default=100)
 parser.add_argument('--save_freq', type=int, default=100)
+parser.add_argument('--smooth_weight', type=float, default=0.1)
+parser.add_argument('--ref_weight', type=float, default=1.0)
+parser.add_argument('--depth_weight', type=float, default=1.0)
+parser.add_argument('--rotation_scale', type=float, default=0.5)
+parser.add_argument('--translation_scale', type=float, default=2.0)
 args = parser.parse_args()
 
 
@@ -36,14 +42,19 @@ def train():
     train_data = dataset.Data(args.data_root, mode='train', device=device)
     # val_data = dataset.Data(args, mode='val')
 
-    train_loader = DataLoader(train_data, batch_size=args.batch_size, shuffle=False)
+    train_loader = DataLoader(train_data, batch_size=args.mini_batch_size, shuffle=False)
     # model = smp.Unet('resnet34', encoder_weights=None, activation='sigmoid')
-    model = Model(args.encoder)
+    model = Model(args.encoder, rotation_scale=args.rotation_scale,
+                  translation_scale=args.translation_scale)
     model = model.to(device)
 
-    optimiser = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    optimiser = torch.optim.SGD(model.parameters(), lr=args.lr)
     writer = SummaryWriter()
-    criteria = Criteria()
+    criteria = Criteria(
+        smooth_weight=args.smooth_weight,
+        ref_weight=args.ref_weight,
+        depth_weight=args.depth_weight
+    )
     pcd = o3d.geometry.PointCloud()
 
     save_dir = os.path.join('checkpoint', args.model_name)
@@ -54,41 +65,37 @@ def train():
 
     for epoch in range(args.epoch_start, args.epoch_start + args.epoch_num):
         model.train()
+        optimiser.zero_grad()
         train_losses = []
-        for data_in in train_loader:
+        for i, data_in in enumerate(train_loader):
             data_out = model(data_in)
             loss = criteria(data_in, data_out)
-            optimiser.zero_grad()
             loss.backward()
             train_losses.append(loss.item())
-            optimiser.step()
+            if (i + 1) % (args.batch_size // args.mini_batch_size) == 0:
+                optimiser.step()
+                optimiser.zero_grad()
 
+        optimiser.step()
         if epoch % args.summary_freq == 0:
             loss = sum(train_losses) / len(train_losses)
             print(epoch, loss)
             util.visualize(data_in)
             util.visualize(data_out)
+            if 'abs_rel' in data_out:
+                writer.add_scalar('eval/abs_rel', data_out['abs_rel'], global_step=epoch)
             writer.add_scalar('loss', loss, global_step=epoch)
             writer.add_image('image/image', data_in['image'][0], global_step=epoch)
-            writer.add_image('image/mask', data_in['mask'][0], global_step=epoch)
+            # writer.add_image('image/mask', data_in['mask'][0], global_step=epoch)
             writer.add_image('image/depth_in', data_in['depth_v'][0], global_step=epoch)
             writer.add_image('image/depth_out', data_out['depth_v'][0], global_step=epoch)
-            for warp in ['warp_stereo', 'warp_previous', 'warp_next']:
-                if warp in data_out:
-                    writer.add_image('image/' + warp,
-                                     data_out[warp][0], global_step=epoch)
-            for motion in ['motion_stereo', 'motion_previous', 'motion_next']:
-                if motion in data_out:
-                    writer.add_text('text/' + motion,
-                                    str(data_out[motion][0].data.cpu().numpy()),
-                                    global_step=epoch)
-
-            # writer.add_image('image/label', label[0], global_step=epoch)
-            # writer.add_image('image/depth', depth[0], global_step=epoch)
-            # writer.add_image('image/predict', output[0], global_step=epoch)
-            # writer.add_image('image/normal', normal[0], global_step=epoch)
-            # writer.add_image('image/overlap', overlap[0], global_step=epoch)
-            # writer.add_image('image/dist', dist[0], global_step=epoch)
+            for key in data_out:
+                if 'residual' in key:
+                    writer.add_image('residual/' + key, data_out[key][0], global_step=epoch)
+                elif 'loss' in key:
+                    writer.add_scalar('loss/' + key, data_out[key], global_step=epoch)
+                elif 'motion' in key:
+                    writer.add_text('motion/' + key, str(data_out[key][0].data.cpu().numpy()), global_step=epoch)
 
         if epoch % args.save_freq == 0:
             os.makedirs(save_dir, exist_ok=True)
@@ -109,8 +116,6 @@ def train():
 
 
 if __name__ == '__main__':
-    # pcd = o3d.geometry.PointCloud()
-    # points = np.random.rand(100, 3)
-    # pcd.points = o3d.utility.Vector3dVector(points)
-    # o3d.io.write_point_cloud("points.ply", pcd)
     train()
+
+
