71a5d83179a034b8fb2f5166ee8d2939e73bdb8e
gagiter <gagiter@163.com>
2020-04-07 21:51:27 +0800

    scale

--- model.py
+++ model.py
@@ -10,6 +10,7 @@ class Vector(torch.nn.Module):
         last_channels = self.net.out_channels[-1]
         self.conv = torch.nn.Conv2d(last_channels, out_channels, kernel_size=3, padding=1)
 
+
     def forward(self, x):
         x = self.net(x)
         x = self.conv(x[-1])
@@ -19,24 +20,35 @@ class Vector(torch.nn.Module):
 
 
 class Model(torch.nn.Module):
-    def __init__(self, encoder='resnet34'):
+    def __init__(self, encoder='resnet34', rotation_scale=1.0, translation_scale=1.0):
         super(Model, self).__init__()
         self.depth_net = smp.Unet(encoder, encoder_weights=None, activation='sigmoid')
         self.camera_net = Vector(encoder, in_channels=3, out_channels=6)
         self.motion_net = Vector(encoder, in_channels=6, out_channels=6)
+        self.rotation_scale = rotation_scale
+        self.translation_scale = translation_scale
 
     def forward(self, data):
         data_out = dict()
         image = data['image']
-        data_out['depth'] = self.depth_net(image)
         # data_out['depth'] = -40.0 * torch.log(data_out['depth'])  # z = exp(-0.025 * d)
-        data_out['depth'] = data_out['depth'] * 100.0  # z = d * 100.0
+        data_out['depth'] = self.depth_net(image) * 100.0  # z = d * 100.0
         data_out['camera'] = self.camera_net(image) * 0.01
+        data_out['camera'] = torch.zeros_like(data_out['camera'])
 
         for ref in ['stereo', 'previous', 'next']:
             if ref in data:
                 image_ref = data[ref]
                 image_stack = torch.cat([image, image_ref], dim=1)
-                data_out['motion_' + ref] = self.motion_net(image_stack) * 0.1
+                motion = self.motion_net(image_stack)
+                if self.rotation_scale > 0.0:
+                    motion[:, 0:3] *= self.rotation_scale
+                else:
+                    motion[:, 0:3] = torch.zeros_like(motion[:, 0:3])
+                if self.translation_scale > 0.0:
+                    motion[:, 3:6] *= self.translation_scale
+                else:
+                    motion[:, 3:6] = torch.zeros_like(motion[:, 3:6])
+                data_out['motion_' + ref] = motion
 
         return data_out
