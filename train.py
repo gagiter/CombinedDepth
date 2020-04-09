58d7bfb8177829e0b284b8c30fd57601a2c0de93
gagiter <gagiter@163.com>
2020-04-09 16:06:15 +0800

    depth five

--- train.py
+++ train.py
@@ -18,9 +18,9 @@ parser.add_argument('--encoder', type=str, default='resnet34')  # mobilenet_v2
 parser.add_argument('--model_name', type=str, default='model')
 parser.add_argument('--batch_size', type=int, default=8)
 parser.add_argument('--mini_batch_size', type=int, default=2)
-parser.add_argument('--resume', action='store_true', default=False)
+parser.add_argument('--resume', type=int, default=1)
 parser.add_argument('--no_cuda', action='store_true', default=False)
-parser.add_argument('--gpu_id', type=int, default=1)
+parser.add_argument('--gpu_id', type=str, default='1')
 parser.add_argument('--lr', type=float, default=0.01)
 parser.add_argument('--momentum', type=float, default=0.5)
 parser.add_argument('--epoch_start', type=int, default=0)
@@ -36,24 +36,22 @@ parser.add_argument('--translation_scale', type=float, default=2.0)
 parser.add_argument('--down_times', type=int, default=4)
 parser.add_argument('--occlusion', type=int, default=1)
 args = parser.parse_args()
+# os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
 
 
 def train():
     use_cuda = torch.cuda.is_available() and not args.no_cuda
-    device = torch.device('cuda:%d' % args.gpu_id if use_cuda else 'cpu')
+    device = torch.device('cuda:' + args.gpu_id if use_cuda else 'cpu')
 
     train_data = dataset.Data(args.data_root, mode='train', device=device)
     # val_data = dataset.Data(args, mode='val')
 
     train_loader = DataLoader(train_data, batch_size=args.mini_batch_size, shuffle=True)
-
-    # model = smp.Unet('resnet34', encoder_weights=None, activation='sigmoid')
     model = Model(args.encoder, rotation_scale=args.rotation_scale,
                   translation_scale=args.translation_scale,
                   depth_scale=args.depth_scale)
     model = model.to(device)
 
-    # optimiser = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
     optimiser = torch.optim.Adam(model.parameters(), lr=args.lr)
     writer = SummaryWriter()
     criteria = Criteria(
@@ -64,9 +62,8 @@ def train():
         occlusion=True if args.occlusion > 0 else False
     )
     pcd = o3d.geometry.PointCloud()
-
     save_dir = os.path.join('checkpoint', args.model_name)
-    if args.resume and os.path.exists(save_dir):
+    if args.resume > 0 and os.path.exists(save_dir):
         model.load_state_dict(torch.load(os.path.join(save_dir, 'model.pth')))
         optimiser.load_state_dict(torch.load(os.path.join(save_dir, 'optimiser.pth')))
         args.epoch_start = torch.load(os.path.join(save_dir, 'epoch.pth'))['epoch']
@@ -92,9 +89,9 @@ def train():
                 writer.add_scalar('eval/abs_rel', data_out['abs_rel'], global_step=epoch)
             writer.add_scalar('loss', loss, global_step=epoch)
             writer.add_image('image/image', data_in['image'][0], global_step=epoch)
-            writer.add_image('image/depth_in', data_in['depth'][0] / 80.0, global_step=epoch)
+            writer.add_image('image/depth_in', data_in['depth'][0] * 5.0, global_step=epoch)
             writer.add_image('image/normal', data_out['normal'][0], global_step=epoch)
-            writer.add_image('image/depth_out', data_out['depth'][0], global_step=epoch)
+            writer.add_image('image/depth_out', data_out['depth'][0] * 5.0, global_step=epoch)
             for key in data_out:
                 if 'residual' in key:
                     writer.add_image('residual/' + key, data_out[key][0], global_step=epoch)
@@ -115,9 +112,6 @@ def train():
             points = points.transpose(1, 2, 0).reshape(-1, 3)
             pcd.points = o3d.utility.Vector3dVector(points)
             o3d.io.write_point_cloud(os.path.join(save_dir, 'points.ply'), pcd)
-
-
-            # torch.save(train_losses, 'train_losses.pth')
 
     writer.close()
 
