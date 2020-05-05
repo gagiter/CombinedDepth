86aea9452fa3dc53be07491a5eb960d8ef45fee9
gagiter <gagiter@163.com>
2020-05-05 12:01:01 +0800

    export camera pt

--- export.py
+++ export.py
@@ -3,7 +3,9 @@ from model import Model
 
 
 model = Model()
-model.load_state_dict(torch.load('checkpoint/sequential_resnet34_10w/model.pth'))
+model.load_state_dict(torch.load('checkpoint/normal_data/model.pth'))
 example = torch.rand(1, 3, 512, 512)
-traced_script_module = torch.jit.trace(model.depth_net, example)
-traced_script_module.save('model_depth.pt')
+traced_script_module_depth = torch.jit.trace(model.depth_net, example)
+traced_script_module_depth.save('model_depth.pt')
+traced_script_module_camera = torch.jit.trace(model.camera_net, example)
+traced_script_module_camera.save('model_camera.pt')
