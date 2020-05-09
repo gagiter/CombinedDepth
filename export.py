90c20cf24295d91126d6454b8fb53a310938979f
gagiter <gagiter@163.com>
2020-05-09 08:12:42 +0800

    encoder

--- export.py
+++ export.py
@@ -5,12 +5,13 @@ import os
 
 parser = argparse.ArgumentParser(description='CombinedDepth')
 parser.add_argument('--model_name', type=str, default='model_name')
+parser.add_argument('--encoder', type=str, default='resnet34')
 parser.add_argument('--save_depth_name', type=str, default='model_depth.pt')
 parser.add_argument('--save_camera_name', type=str, default='model_camera.pt')
 args = parser.parse_args()
 
 checkpoint_name = os.path.join('checkpoint', args.model_name,  'model.pth')
-model = Model()
+model = Model(encoder=args.encoder)
 model.load_state_dict(torch.load(checkpoint_name))
 example = torch.rand(1, 3, 512, 512)
 traced_script_module_depth = torch.jit.trace(model.depth_net, example)
